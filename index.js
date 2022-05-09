e268bd5299d52d044a4b5a8e84d4458461f30123
MarcinBedorf <96438336+MarcinBedorf@users.noreply.github.com>
2022-05-09 20:02:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ class Sprite {
 
     update() {
         this.draw();
-        this.velocity.y += gravity;
+        
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
-        };
+        } else {
+            this.velocity.y += gravity;
+        }
     };
 };
 
@@ -69,3 +72,49 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 1;
+        break
+        case 'a':
+            player.velocity.x = -1;
+        break
+    }
+    
+    console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 0;
+        break
+        case 'a':
+            player.velocity.x = 0;
+        break
+    }
+    
+    console.log(event.key);
+});
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'w':
+            player.velocity.y = -10;
+        break
+    }
+    
+    console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'w':
+            player.velocity.y = 0;
+        break
+    }
+    
+    console.log(event.key);
+});
