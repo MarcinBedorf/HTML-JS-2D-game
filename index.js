df7d67b653d188329c97ff1b4d38823b6fb5a76d
MarcinBedorf <96438336+MarcinBedorf@users.noreply.github.com>
2022-05-09 20:05:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -76,10 +76,10 @@ animate();
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1;
+            player.velocity.x = 5;
         break
         case 'a':
-            player.velocity.x = -1;
+            player.velocity.x = -5;
         break
     }
     
