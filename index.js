3a87fbaacb9814e562d6a29be8da8a6fbfb8f6e1
MarcinBedorf <96438336+MarcinBedorf@users.noreply.github.com>
2022-05-14 20:08:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ class Sprite {
         }
         this.color = color;
         this.isAttacking;
+        this.health = 100;
     };
 
     draw() {
@@ -158,7 +159,8 @@ function animate() {
          }) && player.isAttacking
          ) {
          player.isAttacking = false;
-         console.log('bam');
+         enemy.health -= 20;
+         document.querySelector('#enemyHealth').style.width = enemy.health + '%';
     }
 
     if (
@@ -168,7 +170,8 @@ function animate() {
          }) && enemy.isAttacking
          ) {
          enemy.isAttacking = false;
-         console.log('enemy attack - bam');
+         player.health -= 20;
+         document.querySelector('#playerHealth').style.width = player.health + '%';
     }
 };
 
