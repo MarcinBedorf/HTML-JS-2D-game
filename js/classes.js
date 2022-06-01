50b718c1ef09cb606e65f18f318a183a212564f7
MarcinBedorf <96438336+MarcinBedorf@users.noreply.github.com>
2022-06-01 14:20:32 +0200

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,31 @@
+// background layers class
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 1024;
+        this.height = 576;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = layerSpeed * this.speedModifier;
+    };
+
+    updateLayer() {
+        this.speed = layerSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = 0;
+        }
+        this.x = this.x - this.speed;
+        // this.x = gameFrame * this.speed % this.width;
+    };
+
+    drawLayer() {
+        c.drawImage(this.image, this.x, this.y, this.width, this.height);
+        c.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    };
+};
+
+// sprite class
 class Sprite {
     constructor({
         position,
